fb68edb605e99b4d3d9f016bd63242733b6f5c0b
Arjun Barman <barmanarjun1997@gmail.com>
2022-08-29 21:10:32 +0530

    Add files via upload
    Source : Internet

---
+++ Document vs Window objects.js
@@ -0,0 +1,7 @@
+Document object: When an HTML document is loaded into a window, it becomes a document object. The ‘document’ object has various properties 
+that refer to other objects which allow access to and modification of the content of the web page. If there is a need to access any element 
+in an HTML page, we always start with accessing the ‘document’ object. Document object is property of window object.
+
+Window object: Window Object is object of the browser which is always at top of the DOM hierarchy.  It is like an API that is used to set and 
+access all the properties and methods of the browser. It is automatically created by the browser.Whenever a window appears on the screen to 
+display the contents of document, the window object is created.
